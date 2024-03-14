3e1829a9c05dd49fa210f8a19d687e489666da39
Faheelkhan1204 <153633614+Faheelkhan1204@users.noreply.github.com>
2024-03-14 09:43:16 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,51 @@
+
+// Class=4 9/3/24
+
+let num1=5;
+++ num1;
+console.log(num1)
+let num2=5;
+--num2;
+++num2;
+--num2;
+++num2;
+--num2;
+console.log(num2)
+
+
+let i=10;
+let result=++i + i++ + 6 + i-- -2;
+console.log(i)
+
+
+
+// HOME-WORK 9/3/2024
+let a:number=5;let b:number=2 
+let c:number;
+c=++a + ++a + --b + --b + a + ++b +b 
+console.log(c);
+
+ 
+let num17=2;
+num1+=4;
+console.log(num17)
+let num4=6;
+num4-=4;
+console.log(num4)
+
+let num5=5;
+let num6=2
+console.log(num5==num6)
+
+let num7=5
+let num8=2
+console.log(num7!=num8)
+
+let num9=5;
+let num10=10;
+console.log(num9>num10)
+
+let num11=5;
+let num12=2;
+console.log(num11<num12)
+
---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let numberOfFaculty = 4;
+let numberOfManagement = 3;
+let numberOfAbsentFaculty = 2;
+console.log(numberOfFaculty + numberOfManagement - numberOfAbsentFaculty);
+let numberOfTotalFaculty = numberOfFaculty * 21;
+console.log(numberOfTotalFaculty);
+let toltalNumberOfKgs = 20;
+console.log(toltalNumberOfKgs / numberOfTotalFaculty);
